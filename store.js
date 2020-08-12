d625a4f497c50eb201e255d13f5e94aedf08b75d
Aaron Davis <63382332+AD30344@users.noreply.github.com>
2020-08-12 19:33:31 -0400

    Add files via upload

---
+++ store.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const STORE = {
+  questions: [
+    {
+      question: 'What does Html stand for?', 
+      answers: ["Hyper Text Makeup Language", "Hyper Text Markup Language", "How to make lunch", "Hyper Tone Markup Language"], 
+      correctAnswer: 'Hyper Text Markup Language',
+    }
+  ],
+  quizEnded: false,
+  questionNum: 1,
+  quizScore: 1,
+
+  questions: [
+    {
+      question: 'What is OOP?',
+      answers: ["Optional Object Parameter", "Over Objective Programming", "Object Oriented Programming", "Open Objective Parameter"], 
+      correctAnswer: "Object Oriented Programming"
+    }
+  ],
+  quizEnded: false,
+  questionNum: 2,
+  quizScore: 2,
+
+  questions: [
+    { question: "What type of programming language is Javascript?", 
+    answers: ["Procedural", "Functional", "Object Oriented", "Database"], 
+    correctAnswer: "Object Oriented" }
+  ],
+  quizEnded: false,
+  questionNum: 3,
+  quizScore: 3,
+
+  questions: [
+    { 
+      question: 'What is type of language is Python?', 
+    answers: ["Functional Programming", "Procedural", "Object Oriented Programming", "Database"], 
+    correctAnswer: "Database" }
+  ],
+  quizEnded: false,
+  questionNum: 4,
+  quizScore: 4,
+
+  questions: [
+    { 
+      question: 'What is JQuery?', 
+    answers: ["A Framework", "A Media Query", "A Javascript Library", "A Function"], 
+    correctAnswer: "A Javascript Library" }
+  ],
+  quizEnded: true,
+  questionNum: 5,
+  quizScore: 5,
+}
+  //we need a counter to keep track of which page we are on
+  //how do we move this counter forward when the 
+
+
+    //this is where we should store the object that counts right
+    //answers???
+
+
+
+//instead of having h1 and h2, we could just increase the size
+//of h1, add a class of larger and alter in css.
+//I think that would fix alignment issues
